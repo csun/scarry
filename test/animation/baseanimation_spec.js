b0d4994a7644dfe64686cd95d56636da144043c7
Cameron Sun <cameron.csun@gmail.com>
2015-07-14 12:20:02 -0400

    Added some tests for baseanimation
    Squashed commit of the following:
    
    commit 659c0c38bbe6a9efd703825118aff25de98b0feb
    Author: Cameron Sun <cameron.csun@gmail.com>
    Date:   Tue Jul 14 12:19:39 2015 -0400
    
        Should be a good amount of testing for now
    
    commit cd78e04f0b3553df1bd35cd45517faf29dfab819
    Author: Cameron Sun <cameron.csun@gmail.com>
    Date:   Tue Jul 14 12:12:34 2015 -0400
    
        Starting work on baseanimation tests

--- test/animation/baseanimation_spec.js
+++ test/animation/baseanimation_spec.js
@@ -1,8 +1,71 @@
 var expect = require('chai').expect;
 var BaseAnimation = require('../../src/animation/baseanimation');
 
+// ==================================================================
+// An animation subclass to test that the internal
+// mechanisms of all animations stay consistent and
+// functional.
+var FRAME_TIMING = 1000;
+var TOTAL_FRAMES = 10;
 
+function TestAnimation(options) {
+  this.percentageAdvances = [];
+
+  BaseAnimation.call(this, options);
+}
+TestAnimation.prototype = Object.create(BaseAnimation.prototype);
+
+TestAnimation.prototype._currentFrameTiming = function() {
+  return FRAME_TIMING;
+};
+
+TestAnimation.prototype._totalFrameCount = function() {
+  return TOTAL_FRAMES;
+};
+
+TestAnimation.prototype._advanceFrameByPercentage = function(percentage) {
+  this.percentageAdvances.push(percentage);
+};
+
+// ==================================================================
 
 describe('BaseAnimation', function() {
+  var animation;
 
+  beforeEach(function() {
+    animation = new TestAnimation();
+  });
+
+  it('does not update when the animation has not been started', function() {
+    animation.update(FRAME_TIMING);
+    expect(animation.percentageAdvances.length).to.equal(0);
+  });
+
+  it('updates at the correct percentages', function() {
+    animation.start();
+    animation.update(FRAME_TIMING * 2);
+
+    expect(animation.percentageAdvances.length).to.equal(2);
+    expect(animation.percentageAdvances[0]).to.equal(1);
+    expect(animation.percentageAdvances[1]).to.equal(1);
+  });
+
+  it('will not move more than the max amount of frames', function() {
+    animation.start();
+    animation.update(FRAME_TIMING * TOTAL_FRAMES);
+    animation.update(FRAME_TIMING);
+
+    expect(animation.percentageAdvances.length).to.equal(TOTAL_FRAMES);
+  });
+
+  it('can loop', function() {
+    animation = new TestAnimation({ loop: true });
+    animation.start();
+    
+    animation.update(FRAME_TIMING * TOTAL_FRAMES);
+    expect(animation.percentageAdvances.length).to.equal(TOTAL_FRAMES);
+
+    animation.update(FRAME_TIMING * TOTAL_FRAMES);
+    expect(animation.percentageAdvances.length).to.equal(TOTAL_FRAMES * 2);
+  });
 });
